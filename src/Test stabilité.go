994e95cf390f270423292b776754a9ab3111745a
lucbellicaud <68165378+lucbellicaud@users.noreply.github.com>
2021-10-17 17:14:58 +0200

    Update Test stabilité.go

--- src/Test stabilité.go
+++ src/Test stabilité.go
@@ -7,21 +7,34 @@ import (
 
 func DynamiqueLibre(Elèves []Agent, Universités []Agent, res map[AgentID]AgentID) map[AgentID]AgentID {
 	fmt.Println("Nouveau tour")
+	reverseRes := reverseMap(res)
 	out1:
-		for élèveRes, uniRes := range res {
-			for _,élève := range Elèves {
-				for _,université := range Universités {
-					if élève.PrefersWithID(université.ID, uniRes) && université.PrefersWithID(élèveRes,élève.ID) {
-						fmt.Println("Couple instable :",élèveRes, uniRes)
-						fmt.Println("Elève ",élèveRes, " préfère ", université.ID, " à", uniRes)
-						fmt.Println("Université ",université.ID," préfère ",élèveRes, " à", élève.ID)
-						time.Sleep(time.Second)
-						res[élèveRes] = université.ID
-						res[élève.ID] = uniRes
-						continue out1
-					}
-				}
+	for _,élève := range Elèves {
+		for _,université := range Universités {	
+			if élève.PrefersWithID(université.ID, res[élève.ID]) && université.PrefersWithID(élève.ID,reverseRes[université.ID]) {
+				fmt.Println("Couple instable :",élève.ID, res[élève.ID])
+				fmt.Println("Elève ",élève.ID, " préfère ", université.ID, " à", res[élève.ID])
+				fmt.Println("Université ",université.ID," préfère ",élève.ID, " à", reverseRes[université.ID])
+				time.Sleep(time.Second)
+				res[reverseRes[université.ID]]=res[élève.ID]
+				reverseRes[res[élève.ID]]=reverseRes[université.ID]
+				res[élève.ID]=université.ID
+				reverseRes[université.ID]=élève.ID
+				fmt.Println("res",res)
+				fmt.Println("reverseres",reverseRes)
+
+				continue out1
 			}
 		}
+	}
+	fmt.Println("Fin du dynamique libre")
 	return res
+}
+
+func reverseMap(m map[AgentID]AgentID) map[AgentID]AgentID {
+    n := make(map[AgentID]AgentID, len(m))
+    for k, v := range m {
+        n[v] = k
+    }
+    return n
 }
